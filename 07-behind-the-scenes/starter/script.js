607404af8a87f506352784de53fc93a99ae036f8
infgel <angelfaith1705@gmail.com>
2025-09-24 13:15:16 +0800

    feat(behind-the-scenes): complete Hour 2 with this keyword and arrow functions
    - Add this binding rules for method calls and detached functions
    - Show arrow functions vs regular functions with lexical this behavior
    - Solve common this pitfalls using arrow functions and self = this patterns
    - Compare arguments object in regular functions vs rest parameters
    - Add practical timer examples showing callback this problems and solutions
    - Include comprehensive this binding examples and best practices
    - Master dynamic vs lexical this binding in JavaScript

--- 07-behind-the-scenes/starter/script.js
+++ 07-behind-the-scenes/starter/script.js
@@ -1,59 +1,179 @@
 'use strict';
 console.log('=== BEHIND THE SCENES: SCOPING & HOISTING ===');
 
+// Hour 1: Scoping, Execution Contexts & Hoisting
+
 // SECTION 1
-function alpha() {
-    console.log('alpha:start');
-    beta();
-    console.log('alpha:end');
-}
+// function alpha() {
+//     console.log('alpha:start');
+//     beta();
+//     console.log('alpha:end');
+// }
 
-function beta() {
-    console.log('beta');
-}
+// function beta() {
+//     console.log('beta');
+// }
 
-alpha();
+// alpha();
 
 // SECTION 2
-const outer = 'global';
+// const outer = 'global';
 
-function demoScope() {
-  const inner = 'function';
-  if (true) {
-    const blockConst = 'block';
-    var functionVar = 'var-function-scoped';
-    console.log(outer, inner, blockConst, functionVar);
-  }
-  console.log(outer, inner, functionVar);
-}
+// function demoScope() {
+//   const inner = 'function';
+//   if (true) {
+//     const blockConst = 'block';
+//     var functionVar = 'var-function-scoped';
+//     console.log(outer, inner, blockConst, functionVar);
+//   }
+//   console.log(outer, inner, functionVar);
+// }
 
-demoScope();
+// demoScope();
 
-const name = 'GlobalName';
+// const name = 'GlobalName';
 
-function a() {
-  const name = 'FunctionName';
-  function b() {
-    console.log(name);
-  }
-  b();
-}
+// function a() {
+//   const name = 'FunctionName';
+//   function b() {
+//     console.log(name);
+//   }
+//   b();
+// }
 
-a();
+// a();
 
 // SECTION 3
-var varX = 1;
-let letX = 2;
-const constX = 3;
+// var varX = 1;
+// let letX = 2;
+// const constX = 3;
 
-console.log(varX);
-console.log(letX);
-console.log(constX);
+// console.log(varX);
+// console.log(letX);
+// console.log(constX);
 
-function addDecl(a, b) {
-  return a + b;
-}
-const addExpr = function (a, b) {
-  return a + b;
+// function addDecl(a, b) {
+//   return a + b;
+// }
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+// const addArrow = (a, b) => a + b;
+
+// Hour 2: The this Keyword & Arrow Functions
+// SECTION 1
+// const person = {
+//   name: 'Jonas',
+//   greet: function () {
+//     console.log(`Hello, I am ${this.name}`);
+//   },
+// };
+
+// person.greet();
+// person.name = 'Angel';
+// person.greet();
+
+// const anotherPerson = { name: 'Sarah' };
+// anotherPerson.greet = person.greet;
+// anotherPerson.greet();
+
+// const greetFunction = person.greet;
+// greetFunction();
+
+// const button = document.querySelector('button');
+// button.addEventListener('click', person.greet);
+// button.addEventListener('click', () => person.greet());
+// button.addEventListener('click', person.greet.bind(person));
+
+// SECTION 2
+// const obj = {
+//   name: 'Object',
+
+//   regularMethod: function () {
+//     console.log('Regular:', this.name);
+//   },
+
+//   arrowMethod: () => {
+//     console.log('Arrow:', this.name);
+//   },
+// };
+
+// obj.regularMethod();
+// obj.arrowMethod();
+
+// const quiz = {
+//   name: 'Quiz Object',
+//   regularMethod() {
+//     console.log('Regular:', this.name);
+//   },
+//   arrowMethod: () => {
+//     console.log('Arrow:', this.name);
+//   },
+// };
+
+// quiz.regularMethod();
+// quiz.arrowMethod();
+
+// const timer = {
+//   name: 'Timer',
+
+//   start: function () {
+//     console.log(`${this.name} starting...`);
+//     const self = this;
+
+//     setTimeout(function () {
+//       console.log(`${self.name} finished`);
+//     }, 1000);
+//   },
+
+//   startModern: function () {
+//     console.log(`${this.name} starting modern...`);
+
+//     setTimeout(() => {
+//       console.log(`${this.name} finished modern`);
+//     }, 1500);
+//   },
+// };
+
+// timer.start();
+// timer.startModern();
+
+// const user = {
+//   name: 'Alice',
+//   hobbies: ['reading', 'coding', 'gaming'],
+
+//   printHobbiesBad: () => {
+//     this.hobbies.forEach(hobby => {
+//       console.log(`${this.name} likes ${hobby}`);
+//     });
+//   },
+
+//   printHobbiesGood() {
+//     this.hobbies.forEach(hobby => {
+//       console.log(`${this.name} likes ${hobby}`);
+//     });
+//   },
+// };
+
+// user.printHobbiesBad();
+// user.printHobbiesGood();
+
+const functionTypes = {
+  regularFunction: function () {
+    console.log('Arguments length:', arguments.length);
+    console.log('First argument:', arguments[0]);
+  },
+
+  arrowFunction: () => {
+    console.log('Arrow function called');
+  },
+
+  modernFunction: (...args) => {
+    console.log('Args length:', args.length);
+    console.log('First arg:', args[0]);
+  },
 };
-const addArrow = (a, b) => a + b;
+
+functionTypes.regularFunction('hello', 'world');
+functionTypes.arrowFunction('test');
+functionTypes.modernFunction('modern', 'approach');
